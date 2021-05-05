f2ef2618f1c8657ddc26501c9b6cc65ac9287dc8
jemc300x <jemc300x@gmail.com>
2021-05-04 21:12:14 -0500

    ADDED: onFilter() method

--- src/app/modules/products/pages/index/index.component.ts
+++ src/app/modules/products/pages/index/index.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+interface IProduct {
+  name: string;
+  quantity: number;
+}
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
-export class IndexComponent implements OnInit {
+export class IndexComponent implements OnInit, AfterViewInit {
 
-  products: Array<any> = [];
+  products: Array<IProduct> = [];
+  productsFiltered: Array<IProduct> = [];
+  @ViewChild('txtFilter', {static: true}) inputFilter: ElementRef;
 
   constructor() { }
 
@@ -15,6 +24,21 @@ export class IndexComponent implements OnInit {
     for (let index = 0; index < 100; index++) {
       this.products.push({name: `PRODUCT ${index}`, quantity: index * 2})      
     }
+    this.productsFiltered = this.products;
+  }
+
+  ngAfterViewInit(): void {
+    fromEvent<any>(this.inputFilter.nativeElement, 'keyup')
+      .pipe(debounceTime(400))
+      .subscribe((value) => this.onFilter(String(value.target.value)))
+  }
+
+  onFilter(value: string ): void {
+    if ( value === '') {
+      this.productsFiltered = this.products;
+      return;
+    }
+    this.productsFiltered = this.products.filter( p => p.name.toLowerCase().includes(String(value).toLowerCase()))
   }
 
 }
