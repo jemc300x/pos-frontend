bc0bdb86de7f1ede042f0736ecb249b83fa7bfce
jemc300x <jemc300x@gmail.com>
2021-05-07 21:04:06 -0500

    ADDED: Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', loadChildren: () => import('./modules/home/home.module').then(h => h.HomeModule) },
   { path:'products', loadChildren: () => import('./modules/products/products.module').then(p => p.ProductsModule) },
 ];
 
